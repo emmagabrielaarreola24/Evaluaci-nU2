47d3943f16cf4b2c12dbedbeb75c10f1cb809ffd
emmagabrielaarreola24 <aeve080324mdgrlma4@cbtis130.edu.mx>
2024-10-23 09:45:57 -0600

    Add files via upload
    programa que almacena los precio 5 artículos en un arreglo

---
+++ examen2AArreolaVallejo.php
@@ -0,0 +1,56 @@
+<?php
+/* progrma que imprime los precios de 5 articulos en un arreglo
+cbtis 89 3A 
+programacion matutino 
+arrela vallejo emma gabriela 
+*/
+
+$arrayprecios=array( 500, 400, 200,700,100); 
+echo "<br>";
+echo "ARREGLO PRECIO";
+echo "<br>";
+// se imprime de manera de comentario
+
+$arrayIVA,$precios,$multi;
+for ($precios=0; $precios < ; $precios ++) {
+$prescios= 500, 400 , 200 , 700, 100;
+ $arrayIVA= $precios*.16;
+ $multi = $precios*.16;
+  $multi=$arrayIVA*$arrayprecios;
+  echo "<br>";
+  echo "*ARREGLO DE IVA*";
+  echo "<br>";
+	}
+	// se imprime de manera de comentario
+
+$arrysubtotal,$iva;
+for ($iva=0; $iva < ; $iva++) { 
+	$precios=500,400,200, 700, 100;
+	$precios+$iva;
+	$iva=$arrysubtotal;
+	echo "<br>";
+	echo "*arrelgo Subtotal*";
+	echo "<br>";
+}
+// se imprime de manera de comentario
+$arraydescunto,$iva;
+	for ($iva=0; $iva < ; $iva++) { 
+	$precios=500,400,200, 700, 100;
+	$iva=$precios/10;
+	$arrayIVA/10;
+	$iva=$arraydescuento/$arrayprecios;
+	$iva=$arrayprecios;
+	echo "<br>";
+	echo "*arrelgo descuento*";
+	echo "<br>";
+
+}
+// se imprime de manera de comentario 
+$arrayTotal,$total;
+for ($total=0; $total < ; $total++) { 
+	$arrayIVA+$arraydescuento;
+	echo "*arreglo toral*";
+	echo "<br>";
+	}
+// se imprime de manera de comentario
+?>
